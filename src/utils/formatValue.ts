8fd4c3784bf4a6cf114e184d31bb64dd1fb51f7d
Leonardo Rudolfo Cardoso <leonardorudolfocardoso@gmail.com>
2021-02-03 12:20:20 -0300

    Complete formatValue function to work with both number and string.

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | string): string =>
+  Intl.NumberFormat('Brazil', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(
+    typeof value === 'number' ? value : Number.parseFloat(value)
+    ); // TODO DONE
 
 export default formatValue;
